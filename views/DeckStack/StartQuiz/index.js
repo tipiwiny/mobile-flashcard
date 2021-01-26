47c9a57f36ca7e5e22f6b5f600f0cbd570810d20
Juan Luis Pérez <juanluis.perez.cabrera@bbva.com>
2021-01-26 21:40:39 +0100

    styling quiz view

--- views/DeckStack/StartQuiz/index.js
+++ views/DeckStack/StartQuiz/index.js
@@ -1,6 +1,14 @@
 import React from "react";
-import { View } from "react-native";
-import { Title, Paragraph, Button, IconButton, Subheading } from "react-native-paper";
+import { View, StyleSheet } from "react-native";
+import {
+  Title,
+  Colors,
+  Paragraph,
+  Button,
+  IconButton,
+  Subheading,
+  FAB
+} from "react-native-paper";
 import { connect } from "react-redux";
 
 import { getDeckById } from "../../../actions";
@@ -31,8 +39,8 @@ class StartQuiz extends React.Component {
   onPressResetQuiz() {
     this.setState({
       current: 0,
-      correctAnswers: 0
-    })
+      correctAnswers: 0,
+    });
   }
 
   renderQuiz() {
@@ -41,20 +49,29 @@ class StartQuiz extends React.Component {
     const { showAnswer } = this.state;
     const question = deck ? deck.questions[current] : {};
     return (
-      <View>
-        <Title>{question.question}</Title>
-        <Button onPress={this.onPressShowAnswer.bind(this)}>Answer</Button>
-        {showAnswer ? <Paragraph>{question.answer}</Paragraph> : undefined}
-        <IconButton
-          icon="hand-pointing-up"
-          size={20}
-          onPress={this.onPressCorrect.bind(this)}
-        />
-        <IconButton
-          icon="hand-pointing-down"
-          size={20}
-          onPress={this.onPressNextQuestion.bind(this)}
-        />
+      <View style={styles.container}>
+        <View style={styles.containerTitle}>
+          <Title style={styles.title}>{question.question}</Title>
+        </View>
+        <View style={styles.answerContainer}>
+          <Button  icon="lock-question" mode="text" styles={styles.answerButton} onPress={this.onPressShowAnswer.bind(this)}>Answer</Button>
+          {showAnswer ? <Paragraph>{question.answer}</Paragraph> : undefined}
+        </View>
+        <View style={styles.buttonContainer}>
+            <FAB
+              icon="hand-pointing-up"
+              small
+              style={styles.buttonActions}
+              onPress={this.onPressCorrect.bind(this)}
+            />
+            <FAB
+              icon="hand-pointing-down"
+              small
+              style={styles.buttonActions}
+              onPress={this.onPressNextQuestion.bind(this)}
+            />
+        </View>
+
         <Paragraph>
           {current + 1} / {deck.questions.length} questions
         </Paragraph>
@@ -65,22 +82,70 @@ class StartQuiz extends React.Component {
     const { deck } = this.props;
     const { correctAnswers } = this.state;
     return (
-      <View>
+      <View style={styles.container}> 
+        <View  style={[styles.containerTitle, {alignItems: 'center', color: 'white'}]}>
         <Title>Completed Quiz</Title>
-        <Subheading>Your scoring: {Math.round((correctAnswers/(deck.questions.length))* 100)} %</Subheading>
-        <Button onPress={this.onPressResetQuiz.bind(this)}>Reset Quiz</Button>
-        <Button onPress={() => this.props.navigation.goBack()}>Go to Back</Button>
+        <Subheading>
+          Your scoring:{" "}
+          {Math.round((correctAnswers / deck.questions.length) * 100)} %
+        </Subheading>
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '100%', margin: 20, paddingHorizontal: 20}}>
+        <Button mode="contained" onPress={this.onPressResetQuiz.bind(this)}>Reset Quiz</Button>
+        <Button  mode="contained" onPress={() => this.props.navigation.goBack()}>
+          Go to Back
+        </Button>
+        </View>
+
       </View>
     );
   }
   render() {
     const { deck } = this.props;
     const { current } = this.state;
-    const view = current >= deck.questions.length ? this.renderCompletedQuiz(): this.renderQuiz()
-    return view
+    const view =
+      current >= deck.questions.length
+        ? this.renderCompletedQuiz()
+        : this.renderQuiz();
+    return view;
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  containerTitle: {
+    backgroundColor: Colors.blue500,
+    width: "100%",
+    flex: 4,
+    justifyContent: "center",
+  },
+  title: {
+    color: "white",
+    textAlign: "center",
+  },
+  answerContainer:{
+    width: "100%",
+    flex: 2,
+    position : 'relative',
+    alignItems: 'center'
+  },
+  answerButton: {
+    width: '50%', flex: 1, position: 'absolute' , top: '50%' , left: 10
+  },
+  answer:{
+    width: '50%',
+    textAlign: 'center', flex: 1
+  },
+  buttonContainer : {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    padding: 20
+  },
+});
 function mapStateToProps({ decks }) {
   return {
     deck: decks.deck,
